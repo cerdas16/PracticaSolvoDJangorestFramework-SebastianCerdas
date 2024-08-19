ea21e6dc79c19647b3268f1e227990dcf03c12b5
Sebastian Cerdas <sebastian.cerdas.quiros.lte38@gmail.com>
2024-08-19 11:14:17 +0200

    dacreación de modelos y instalación gentelella

--- src/ATM/models.py
+++ src/ATM/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class client(models.Model):
+
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    card_pin = models.CharField(max_length=4)
+
+class account(models.Model):
+
+    client = models.ForeignKey(client, on_delete=models.CASCADE)
+    bank_fund = models.DecimalField(max_digits=40, decimal_places=2)
+
+class office_user(models.Model):
+
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
