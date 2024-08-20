c5c5fdade135bf91e23e40ea3fc18a37bb6373b7
Sebastian Cerdas <sebastian.cerdas.quiros.lte38@gmail.com>
2024-08-20 09:58:20 +0200

    parte 3 de metodos para retiro

--- src/ATM/models.py
+++ src/ATM/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class client(models.Model):
@@ -8,10 +8,16 @@ class client(models.Model):
     password = models.CharField(max_length=50)
     card_pin = models.CharField(max_length=4)
 
+    def __str__(self):
+        return self.card_pin
+
+
 class account(models.Model):
 
     client = models.ForeignKey(client, on_delete=models.CASCADE)
     bank_fund = models.DecimalField(max_digits=40, decimal_places=2)
+    def __str__(self):
+        return f"account {self.id}"
 
 class office_user(models.Model):
 
@@ -20,6 +26,9 @@ class office_user(models.Model):
 
 class binnacle(models.Model):
 
-    account = models.ForeignKey(account, on_delete=models.CASCADE)
-    withdrawal_amount = models.CharField(max_length=20)
-    password = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(default=timezone.now)
+    action = models.CharField(max_length=255)
+    details = models.TextField()
+
+    def __str__(self):
+        return f"{self.timestamp} - {self.action}"
