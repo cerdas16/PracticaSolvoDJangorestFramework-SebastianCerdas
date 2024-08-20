cad0e73eaa95722b8547e958d6ec47fb4b11dbfc
Sebastian Cerdas <sebastian.cerdas.quiros.lte38@gmail.com>
2024-08-20 16:48:18 +0200

    parte 5- form y vistas para gestionar clients

--- src/ATM/models.py
+++ src/ATM/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.user.username
