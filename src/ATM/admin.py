0dae98f5b81cfe8b93408c92a1dc108022548d07
Sebastian Cerdas <sebastian.cerdas.quiros.lte38@gmail.com>
2024-08-20 15:05:03 +0200

    parte 4 de ATM

--- src/ATM/admin.py
+++ src/ATM/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import account, client, office_user, binnacle
+from .models import Account, Client, Office_User, Binnacle
 
 # Register your models here.
 
-admin.site.register(client)
-admin.site.register(account)
-admin.site.register(office_user)
-admin.site.register(binnacle)
+admin.site.register(Client)
+admin.site.register(Account)
+admin.site.register(Office_User)
+admin.site.register(Binnacle)
--- src/ATM/models.py
+++ src/ATM/models.py
@@ -1,30 +1,30 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
-class client(models.Model):
-
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    card_pin = models.CharField(max_length=4)
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.card_pin
+        return self.user.username
 
+class Account(models.Model):
 
-class account(models.Model):
-
-    client = models.ForeignKey(client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
     bank_fund = models.DecimalField(max_digits=40, decimal_places=2)
+    card_pin = models.CharField(max_length=4)
     def __str__(self):
         return f"account {self.id}"
 
-class office_user(models.Model):
 
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=50)
+class Office_User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class binnacle(models.Model):
+    def __str__(self):
+        return self.user.username
+
+class Binnacle(models.Model):
 
     timestamp = models.DateTimeField(default=timezone.now)
     action = models.CharField(max_length=255)
